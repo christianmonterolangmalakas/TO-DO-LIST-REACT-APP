617530ff8cd0c12c15a443d30eeedc62ea7cb495
christian montero <christianopogito123@gmail.com>
2023-12-28 14:27:43 +0800

    update css and js

--- App.js
+++ App.js
@@ -1,83 +1,119 @@
-import React, { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css"; //npm install bootstrap
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 
+const getRandomColor = () => {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
 const TodoApp = () => {
-  const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState("");
-  const [selectedTask, setSelectedTask] = useState(null);
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
+  const [containerColor, setContainerColor] = useState(getRandomColor());
+  const [addButtonColor, setAddButtonColor] = useState(getRandomColor());
+  const [deleteButtonColor, setDeleteButtonColor] = useState(getRandomColor());
 
-  const handleCreate = () => {
-    const newTaskWithSequence = `Task ${tasks.length + 1}: ${newTask}`;
-    setTasks([...tasks, newTaskWithSequence]);
-    setNewTask("");
+  const bodyRef = useRef(document.body);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const newContainerColor = getRandomColor();
+      setContainerColor(newContainerColor);
+      setAddButtonColor(getRandomColor());
+      setDeleteButtonColor(getRandomColor());
+
+      
+      bodyRef.current.style.backgroundColor = getRandomColor();
+    }, 4000); 
+
+    return () => clearInterval(intervalId); 
+  }, []);
+
+  const addTodo = () => {
+    if (editIndex !== null) {
+      const updatedTodos = [...todos];
+      updatedTodos[editIndex] = {
+        text: newTodo,
+        color: todos[editIndex].color,
+      };
+      setTodos(updatedTodos);
+      setEditIndex(null);
+    } else if (newTodo.trim() !== "") {
+      const newColor = getRandomColor();
+      setTodos([...todos, { text: newTodo, color: newColor }]);
+      setContainerColor(getRandomColor());
+      setAddButtonColor(getRandomColor());
+      setDeleteButtonColor(getRandomColor());
+
+     
+      bodyRef.current.style.backgroundColor = getRandomColor();
+    }
+    setNewTodo("");
   };
 
-  const handleUpdate = () => {
-    if (selectedTask !== null && newTask.trim() !== "") {
-      setTasks((prevTasks) =>
-        prevTasks.map((task, index) =>
-          index === selectedTask ? `Task ${selectedTask + 1}: ${newTask}` : task
-        )
-      );
-
-      setNewTask("");
-      setSelectedTask(null);
-    }
+  const editTodo = (index) => {
+    setNewTodo(todos[index].text);
+    setEditIndex(index);
   };
 
-  const handleDelete = () => {
-    if (selectedTask !== null) {
-      setTasks((prevTasks) =>
-        prevTasks
-          .filter((_, index) => index !== selectedTask)
-          .map(
-            (task, index) => `Task ${index + 1}: ${task.split(":")[1].trim()}`
-          )
-      );
-      setNewTask("");
-      setSelectedTask(null);
-    }
+  const deleteTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+    setEditIndex(null);
+    setContainerColor(getRandomColor());
+    setAddButtonColor(getRandomColor());
+    setDeleteButtonColor(getRandomColor());
+
+   
+    bodyRef.current.style.backgroundColor = getRandomColor();
   };
 
   return (
-    <div className="container mt-5">
-      <h1>To-Do List Application</h1>
-      <div className="mb-3">
+    <div className="container" style={{ backgroundColor: containerColor }}>
+      <h1>Montero's To-Do List Application</h1>
+      <div className="input-container">
         <input
           type="text"
-          className="form-control"
-          placeholder="Enter task"
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
         />
-      </div>
-      <div className="mb-3">
-        <button className="btn btn-primary mr-2" onClick={handleCreate}>
-          Create
-        </button>
-        <button className="btn btn-warning mr-2" onClick={handleUpdate}>
-          Update
-        </button>
-        <button className="btn btn-danger" onClick={handleDelete}>
-          Delete
+        <button onClick={addTodo} style={{ backgroundColor: addButtonColor }}>
+          {editIndex !== null ? "Update" : "Add"}
         </button>
       </div>
-      <div className="task-list-container">
-        <ul className="list-group task-list">
-          {tasks.map((task, index) => (
-            <li
-              key={index}
-              className={`list-group-item ${
-                selectedTask === index ? "active" : ""
-              }`}
-              onClick={() => setSelectedTask(index)}
-            >
-              {task}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li
+            key={index}
+            className="todo-item"
+            style={{ backgroundColor: todo.color }}
+          >
+            {todo.text}
+            <div className="button-container">
+              <button
+                className="edit-button"
+                onClick={() => editTodo(index)}
+                style={{ backgroundColor: addButtonColor }}
+              >
+                Edit
+              </button>
+              <button
+                className="delete-button"
+                onClick={() => deleteTodo(index)}
+                style={{ backgroundColor: deleteButtonColor }}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
